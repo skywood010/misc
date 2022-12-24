c5a5acfe7f73930490319ef898417cfc9e91e206
skwlkr21 <skywood010@gmail.com>
2022-12-24 17:15:54 +0600

    added arguments

--- test_random.py
+++ test_random.py
@@ -1,7 +1,8 @@
 import random
 
-def create_random_num():
-    return random.randint(0, 20)
+def create_random_num(a, b):
+    return random.randint(a, b)
+
 
 if __name__ == "__main__":
     print(create_random_num())
