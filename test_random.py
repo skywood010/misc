9139b1afba738e92402ab627c2a69cd7808884d1
skwlkr21 <skywood010@gmail.com>
2022-12-24 17:23:58 +0600

    first commit
    Signed-off-by: skwlkr21 <skywood010@gmail.com>

--- test_random.py
+++ test_random.py
@@ -3,7 +3,11 @@ import random
 def create_random_num(a, b):
     return random.randint(a, b)
 
+def new_function():
+    return "hello"
+
 
 if __name__ == "__main__":
     print(create_random_num())
+    print(new_function())
 
